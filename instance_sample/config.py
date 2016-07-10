8684a69d021ea85ddf2fd9464c3ca37072462d3f
minekoa <mineko.orange@gmail.com>
2016-07-10 21:28:06 +0900

    instance_sample フォルダを追加。instanceにリネームするととりあえずすぐにつかえるハズ

---
+++ instance_sample/config.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+CONTENTS_DIR  = './instance_sample/contents/'
+SITE_NAME     = 'esehatena'
+WORLD_DICT    = './instance_sample/worlddict.txt'
+STYLE_SHEET   = './instance_sample/stylesheet.css'
+DISPLAY_PAGES = 3
+
+
+
